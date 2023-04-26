2f101d152c6fb9d07b6dcb8d02e60d070851c4aa
Matt Collins <mcollins24@hanoverstudents.org>
2023-04-26 12:46:27 -0400

    wow9r458cd

--- web/index.php
+++ web/index.php
@@ -7,8 +7,8 @@ include('library.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="macStyles.css">
     <link rel="icon" href="mac_logo.png">
     <title>MAC</title>
 
@@ -16,11 +16,15 @@ include('library.php');
 <body>
     <!-- Standard navigation buttons for all pages -->
     <div class = "center">
-            <img src="mac_logo.png" alt="MAC EVERYTHING" width="60px" length="60px">
+            <img src="mac_logo.png" alt="MAC EVERYTHING" width="100px" length="100px"></img>
+            <h2>M.A.C.<h2>
             <span class = "button"><a id = "link"  href="index.php?content=home">Home</a></span>
             <span class = "button"><a id = "link"  href="index.php?content=recipes">Recipes</a></span>
             <span class = "button"><a id = "link"  href="index.php?content=statistics">Stats</a></span>
-            <span><a id = "link"  href="index.php?content=settings"><i class="fa-regular fa-gear"></i></a></span>
+            <span><a href="index.php?content=settings"><i class="fa-regular fa-gear"></i></a></span>
+            <br />
+            <br />
+
             <?php include(get_content() . '.php'); ?>
     </div>
 </body>
