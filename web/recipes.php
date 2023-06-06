5b4791d7865f48a4713256fce532e5cf87e35dfc
Matt Collins <mcollins24@hanoverstudents.org>
2023-06-06 11:20:37 -0400

    poduxudufkl7ttdfgt

--- web/recipes.php
+++ web/recipes.php
@@ -111,15 +111,66 @@ while ($row = $result->fetch_assoc())
             echo "&nbsp &nbsp" . "Cheeese(s): " . $cheeseList;
             echo "<br />" . "<br />";
             echo "&nbsp &nbsp" . "Description: " . $row['rec_desc'];
+
+            $userID = $_SESSION['userId'];
+            $RUsql = <<<SQL
+            SELECT *
+            FROM recipe_user_join
+            WHERE ruj_rec_id = $rID AND ruj_user_id = $userID
+            SQL;
+
+            $RUresult = mysqli_query($dbh, $RUsql);
+            $RUcount = mysqli_num_rows($RUresult);
+
+
+
+
+
+            echo"<div class='container col-md-6 col-md-offset-3'>";
+            echo "<input type='submit' id='r" . $row['rec_id'] . "' class='like-btn";
+            if($RUcount == 1){
+                echo "-selected";
+            } 
+            echo "' value='Like' onclick='like(" . $row['rec_id'] . ")'' />";
+            echo "</div>";
+
+
             echo "</div>";
             echo "<br />";
+            
+            
+
         }
-
-
 ?>
-
 <br />
 
+
+<script>
+    
+function like(recID){
+
+    var settings = {
+            'async': true,
+            'url': 'userRecord.php?user_password=' + $('#user_password').val() + '&name=' + $('#displayName').val() + '&userID=' + <?php echo $_SESSION['userId'] ?> + '&email=null@null.null' + '&favCheId=' + $('#cheese').val() + '&update=true',
+            'method': 'POST',
+            'headers': {
+                'Cache-Control': 'no-cache'
+            }
+        };
+
+        $('#r'+ recID).prop('disabled', true);
+        $.ajax(settings).done(function(response) {
+            setTimeout(function() { window.location.replace('index.php?content=recipes'); }, 500);
+        }).fail(function(jqXHR) {
+            alert('Oops, Error! - Something went wrong, try again later.');
+        }).always(function() {
+            $('#r'+ recID).prop('disabled', false);
+        });
+
+}
+
+
+</script>
 
 
 
